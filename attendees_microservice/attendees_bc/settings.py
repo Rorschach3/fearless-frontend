9135eb9dc999922189950f09ff180a8bed641dd6
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-04-17 18:21:29 -0700

    Submitting the Form

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -56,6 +56,21 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8001",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 CRONJOBS = [
--- monolith/conference_go/settings.py
+++ monolith/conference_go/settings.py
@@ -64,6 +64,21 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3001",
+    "http://127.0.0.1:8001",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = "conference_go.urls"
@@ -139,6 +154,6 @@ DJWTO_MODE = "TWO-COOKIES"
 
 DJWTO_ACCESS_TOKEN_LIFETIME = None
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
 CORS_ALLOW_CREDENTIALS = True
