269c91658c5e21c342576158b431d569f912be12
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-26 19:52:01 -0700

    data to DOM

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,56 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, description, pictureUrl, location) {
+    return `
+        <div class="col">
+        <div class="card h-100">
+          <img src="${pictureUrl}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
+            <p class="card-text">${description}</p>
+            <div class="card-footer">
+  
+          </div>
+        </div>         
+    `;
+}
+
+  
 
 
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        //handle error response
+            throw new Error('Response not OK');
+
+        } else {
+            const data = await response.json();
+      
+            for (let conference of data.conferences) {
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
+                
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    console.log(details);
+                    const name = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    // const end = details.conference.end;
+                    // const starts = details.conference.starts;
+                    const location = details.conference.location.name;
+                    const html = createCard(name, description, pictureUrl, location);
+                    const cardContainer = document.querySelector('.card-container');
+                    cardContainer.innerHTML += html;
+                }
+              }
+            }
+    } catch (error) {
+    console.error("Error:", error);
+    }
 });
+      
--- monolith/events/api_urls.py
+++ monolith/events/api_urls.py
@@ -5,16 +5,19 @@ from .api_views import (
     api_list_locations,
     api_show_conference,
     api_show_location,
+    api_list_states,
 )
 
 
 urlpatterns = [
+    path("locations/", api_list_locations, name="api_list_locations"),
+    path("locations/<int:pk>/", api_show_location, name="api_show_location"),
+    path("states/", api_list_states, name="api_list_states"),
+
     path("conferences/", api_list_conferences, name="api_list_conferences"),
     path(
         "conferences/<int:pk>/",
         api_show_conference,
         name="api_show_conference",
     ),
-    path("locations/", api_list_locations, name="api_list_locations"),
-    path("locations/<int:pk>/", api_show_location, name="api_show_location"),
 ]
