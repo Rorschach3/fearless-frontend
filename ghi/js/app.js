e31a400361b28657dd5a971e6b5698ae45ca4e3c
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-27 06:34:18 -0700

    Date formatted

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,22 +1,28 @@
-function createCard(name, description, pictureUrl, location) {
+function createCard(name, description, pictureUrl, starts, ends, location) {
+    const startDate = new Date(starts);
+    const newStartDate = startDate.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+    const endDate = new Date(ends);
+    const newEndDate = endDate.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
     return `
+    <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
-        <div class="card h-100">
-          <img src="${pictureUrl}" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
-            <p class="card-text">${description}</p>
-            <div class="card-footer">
-  
-          </div>
-        </div>         
+            <div class="card">
+            <img src="${pictureUrl}" class="card-img-top shadow-lg p-3 mb-5 bg-white rounded">
+            <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
+                        <p class="card-text">${description}</p>
+                    <div class="card-footer">${newStartDate} - ${newEndDate}                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>       
     `;
 }
 
+
   
-
-
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -24,12 +30,13 @@ window.addEventListener('DOMContentLoaded', async () => {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-        //handle error response
+            //handle error response
             throw new Error('Response not OK');
-
+            
         } else {
             const data = await response.json();
-      
+            const column = document.querySelector('.col');
+            
             for (let conference of data.conferences) {
                 const detailUrl = `http://localhost:8000${conference.href}`;
                 const detailResponse = await fetch(detailUrl);
@@ -37,20 +44,20 @@ window.addEventListener('DOMContentLoaded', async () => {
                 if (detailResponse.ok) {
                     const details = await detailResponse.json();
                     console.log(details);
-                    const name = details.conference.name;
+                    const title = details.conference.name;
                     const description = details.conference.description;
                     const pictureUrl = details.conference.location.picture_url;
-                    // const end = details.conference.end;
-                    // const starts = details.conference.starts;
+                    const ends = details.conference.ends;
+                    const starts = details.conference.starts;
                     const location = details.conference.location.name;
-                    const html = createCard(name, description, pictureUrl, location);
-                    const cardContainer = document.querySelector('.card-container');
-                    cardContainer.innerHTML += html;
+                    const html = createCard(title, description, pictureUrl, starts, ends, location);
+                    console.log(column);
+                    column.innerHTML += html;
                 }
-              }
             }
-    } catch (error) {
-    console.error("Error:", error);
+        }
+    } catch (e) {
+        console.error(e);
     }
 });
       
